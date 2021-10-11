23ff99a765b2eed090726bab79144ac775fa32d9
Uladzislau <logentuch@gmail.com>
2021-10-11 13:15:25 +0300

    Add project files.

---
+++ Main Project/Controllers/SearchController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using E_commerceFirstFull.Models;
+using E_commerceFirstFull.Services;
+using System.Linq;
+using E_commerceFirstFull.Models.ViewModels;
+using System.Collections.Generic;
+
+namespace E_commerceFirstFull.Controllers
+{
+    public class SearchController : Controller
+    {
+        private IProductRepository<Product> repository;
+        private ProductService productService;
+        private int totalItems = 0;        
+
+        public SearchController(IProductRepository<Product> productRepository, ProductService productService)
+        {
+            this.productService = productService;
+            this.repository = productRepository;
+        }        
+
+        public IActionResult Index(string searchQuery, string searchForm, RequestParameters request)
+        {
+            if (searchForm != null)
+                productService.SearchQueryCached = searchQuery;            
+
+            var selectedProducts = repository.GetByTitle(productService.SearchQueryCached);
+            productService.GetListOfCategories(request.Genre, request.Features, request.Platform);
+
+            if (productService.SelectedCategoriesCached.Count == 0)
+                totalItems = selectedProducts.Count();
+
+            selectedProducts = selectedProducts.Where(x => productService.SelectedCategoriesCached
+                                               .All(x.Features.Contains));
+
+            if (productService.SelectedCategoriesCached.Count != 0)
+                totalItems = selectedProducts.Count();
+
+            selectedProducts = selectedProducts.Skip((request.PageNumber - 1) * request.PageSize)
+                                               .Take(request.PageSize);
+
+            productService.SortProduct(ref selectedProducts, request.SortOrder);            
+
+            
+            return View(Map(request.PageNumber, totalItems, productService.SortObjectCached, selectedProducts));            
+        }        
+
+        private ProductModel Map(int pageNumber, int totalItems, string sortObjectCached, 
+            IEnumerable<Product> selectedProducts)
+        {
+            ProductModel productModel = new()
+            {
+                CurrentPage = pageNumber,
+                TotalItems = totalItems,
+                SortMethod = sortObjectCached,
+                Products = selectedProducts.ToList()
+            };
+
+            return productModel;
+        }
+
+        public IActionResult ProductPage()
+        {
+            return View();
+        }
+    }
+}
